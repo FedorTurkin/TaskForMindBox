a68c02cfb7bd56617c5ece74de4952d634e8684a
FedorTurkin <fedorturkin7@gmail.com>
2023-08-27 12:42:34 +0300

    Library + Query
    Task1(Library) + Task2(Query)

---
+++ AreaLibrary/AreaCalculation.cs
@@ -0,0 +1,29 @@
+﻿namespace AreaLibrary;
+public static class AreaCalculation
+{
+    public static double CirclAarea(double R)
+    {
+        double area;
+        area = double.Pi*R*R;
+        return area;
+    }
+    public static double TriangleArea(double a, double b, double c)
+    {
+        double area = 0;
+        if (a+b>c)
+        {
+            if (b + c > a)
+            {
+                if (a + c > b)
+                {
+                    double p = (a + b + c) / 2;
+                    area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                }
+               
+            }
+            
+        }
+        return area;
+    }
+}
+
